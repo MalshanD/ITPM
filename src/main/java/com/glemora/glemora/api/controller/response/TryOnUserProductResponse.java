ac2601ab94427279ed0d6d47464fcb6f658daec3
JanendraGit <janendra.silva2001@gmail.com>
2025-05-08 11:45:17 +0530

    try on response update1

--- src/main/java/com/glemora/glemora/api/controller/response/TryOnUserProductResponse.java
+++ src/main/java/com/glemora/glemora/api/controller/response/TryOnUserProductResponse.java
@@ -5,4 +5,8 @@ import lombok.Data;
 @Data
 public class TryOnUserProductResponse {
 
+    private String generatedImageUrl;
+    private Long productId;
+    private Long userId;
+    private String uploadedImagePath;
 }
