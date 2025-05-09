d63f87f1b0b636e2a91089c7c36d334be90da5d0
JanendraGit <janendra.silva2001@gmail.com>
2025-05-09 13:32:17 +0530

    add virtual try on service update1

--- src/main/java/com/glemora/glemora/api/service/Impl/VirtualTryOnServiceImpl.java
+++ src/main/java/com/glemora/glemora/api/service/Impl/VirtualTryOnServiceImpl.java
@@ -1,11 +1,23 @@
 package com.glemora.glemora.api.service.Impl;
 
+import com.cloudinary.Cloudinary;
+import com.glemora.glemora.api.config.PixelcutConfig;
+import com.glemora.glemora.api.repository.VirtualTryOnImageRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class VirtualTryOnServiceImpl {
+
+    private final PixelcutConfig properties;
+    private final RestTemplate restTemplate;
+    private final Cloudinary cloudinary;
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
+    private final VirtualTryOnImageRepository virtualTryOnImageRepository;
+
 }
