1275887c14c82e552fc96212eea29b95e6af4a82
KavinJ3344 <152595436+KavinJ3344@users.noreply.github.com>
2025-05-09 19:51:40 +0530

    update the product repository file

--- src/main/java/com/glemora/glemora/api/repository/ProductRepository.java
+++ src/main/java/com/glemora/glemora/api/repository/ProductRepository.java
@@ -3,5 +3,5 @@ git package com.glemora.glemora.api.repository;
 import com.glemora.glemora.api.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public  ProductRepository extends JpaRepository<Product, Long> {
 }
