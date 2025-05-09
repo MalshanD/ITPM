b9d1bd00d976c918948b72de88d39e3806a1fe97
JanendraGit <janendra.silva2001@gmail.com>
2025-05-09 13:31:33 +0530

    add virtual try on service update

--- src/main/java/com/glemora/glemora/api/service/Impl/VirtualTryOnServiceImpl.java
+++ src/main/java/com/glemora/glemora/api/service/Impl/VirtualTryOnServiceImpl.java
@@ -1,4 +1,11 @@
 package com.glemora.glemora.api.service.Impl;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class VirtualTryOnServiceImpl {
 }
