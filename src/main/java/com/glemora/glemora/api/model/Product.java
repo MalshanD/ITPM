c713fed5ae499c0535157261eaac4b99ac25b7c8
KavinJ3344 <152595436+KavinJ3344@users.noreply.github.com>
2025-05-09 19:35:22 +0530

    upadte the product file @ models

--- src/main/java/com/glemora/glemora/api/model/Product.java
+++ src/main/java/com/glemora/glemora/api/model/Product.java
@@ -38,5 +38,13 @@ public class Product {
     @Column(columnDefinition = "boolean default false")
     private Boolean featured = false;
 
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
 
 }
