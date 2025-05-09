70242f126d0f83269f6f5603b78081449088d410
JanendraGit <janendra.silva2001@gmail.com>
2025-05-09 14:00:25 +0530

    add virtual try on controller update3

--- src/main/java/com/glemora/glemora/api/controller/VirtualTryOnController.java
+++ src/main/java/com/glemora/glemora/api/controller/VirtualTryOnController.java
@@ -6,11 +6,13 @@ import com.glemora.glemora.api.service.Impl.VirtualTryOnServiceImpl;
 import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/tryon")
@@ -24,6 +26,17 @@ public class VirtualTryOnController {
     @RolesAllowed({"USER", "ADMIN"})
     public ResponseEntity<TryOnResponse> tryOnGarment(@RequestBody TryOnRequest request) {
         TryOnResponse response = virtualTryOnService.tryOnGarment(request.getPersonImageUrl(), request.getGarmentImageUrl());
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @RolesAllowed({"USER", "ADMIN"})
+    public ResponseEntity<TryOnResponse> tryOnGarmentWithUpload(@RequestParam("personImage") MultipartFile personImage, @RequestParam("garmentImage") MultipartFile garmentImage) throws IOException {
+
+        log.info("Received file uploads for try-on: person image size: {}, garment image size: {}", personImage.getSize(), garmentImage.getSize());
+
+        Map<String, String> uploadedImages = virtualTryOnService.uploadAndResizeImages(personImage, garmentImage);
+        TryOnResponse response = virtualTryOnService.tryOnGarment(uploadedImages.get("personImageUrl"), uploadedImages.get("garmentImageUrl"));
         return ResponseEntity.ok(response);
     }
 }
