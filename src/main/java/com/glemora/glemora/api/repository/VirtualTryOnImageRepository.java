80ef0b23f4830dcf521c6404b73df0ef12fcaa3c
JanendraGit <janendra.silva2001@gmail.com>
2025-05-09 10:15:48 +0530

    add repo

---
+++ src/main/java/com/glemora/glemora/api/repository/VirtualTryOnImageRepository.java
@@ -0,0 +1,12 @@
+package com.glemora.glemora.api.repository;
+
+import com.glemora.glemora.api.model.Product;
+import com.glemora.glemora.api.model.User;
+import com.glemora.glemora.api.model.VirtualTryOnImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface VirtualTryOnImageRepository extends JpaRepository<VirtualTryOnImage, Long> {
+
+}
