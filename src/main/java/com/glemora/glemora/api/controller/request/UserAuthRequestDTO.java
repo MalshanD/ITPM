9100354e1bea7e78cf1ff62096f20c823998f729
IT22315328 <159462114+IT22315328@users.noreply.github.com>
2025-05-09 21:26:54 +0530

    fix user-auth-request

--- src/main/java/com/glemora/glemora/api/controller/request/UserAuthRequestDTO.java
+++ src/main/java/com/glemora/glemora/api/controller/request/UserAuthRequestDTO.java
@@ -1,4 +1,4 @@
-git package com.glemora.glemora.api.controller.request;
+package com.glemora.glemora.api.controller.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
