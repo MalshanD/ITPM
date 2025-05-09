c39d5d6c1c8bb484535533721b1c6aaf9bd853eb
JanendraGit <janendra.silva2001@gmail.com>
2025-05-09 14:10:00 +0530

    add security update6

--- src/main/java/com/glemora/glemora/api/security/SecurityConfiguration.java
+++ src/main/java/com/glemora/glemora/api/security/SecurityConfiguration.java
@@ -1,4 +1,18 @@
 package com.glemora.glemora.api.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true) // Enable @RolesAllowed
+@RequiredArgsConstructor
 public class SecurityConfiguration {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final CorsFilter corsFilter;
 }
