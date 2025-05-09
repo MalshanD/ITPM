01e870e42bb36fee14b246637e761b1d98b707d9
JanendraGit <janendra.silva2001@gmail.com>
2025-05-09 13:59:47 +0530

    add virtual try on controller update2

--- src/main/java/com/glemora/glemora/api/controller/VirtualTryOnController.java
+++ src/main/java/com/glemora/glemora/api/controller/VirtualTryOnController.java
@@ -1,8 +1,14 @@
 package com.glemora.glemora.api.controller;
 
+import com.glemora.glemora.api.controller.request.TryOnRequest;
+import com.glemora.glemora.api.controller.response.TryOnResponse;
 import com.glemora.glemora.api.service.Impl.VirtualTryOnServiceImpl;
+import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class VirtualTryOnController {
 
     private final VirtualTryOnServiceImpl virtualTryOnService;
+
+    @PostMapping
+    @RolesAllowed({"USER", "ADMIN"})
+    public ResponseEntity<TryOnResponse> tryOnGarment(@RequestBody TryOnRequest request) {
+        TryOnResponse response = virtualTryOnService.tryOnGarment(request.getPersonImageUrl(), request.getGarmentImageUrl());
+        return ResponseEntity.ok(response);
+    }
 }
