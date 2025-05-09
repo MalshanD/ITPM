39016c3530f7a0233792a04f28c5eb08f125bbcb
IT22315328 <159462114+IT22315328@users.noreply.github.com>
2025-05-09 21:29:53 +0530

    fix userlogin-request

--- src/main/java/com/glemora/glemora/api/controller/request/UserLoginRequest.java
+++ src/main/java/com/glemora/glemora/api/controller/request/UserLoginRequest.java
@@ -1,4 +1,4 @@
-gitpackage com.glemora.glemora.api.controller.request;
+package com.glemora.glemora.api.controller.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
