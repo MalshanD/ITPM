d35e6b2b129d19b25a7a06454e138e5c755f1e6d
MalshanD <malshanshalinda115@gmail.com>
2025-05-10 16:44:07 +0530

    final

--- src/main/java/com/glemora/glemora/api/repository/CartRepository.java
+++ src/main/java/com/glemora/glemora/api/repository/CartRepository.java
@@ -12,4 +12,5 @@ public interface CartRepository extends JpaRepository<Cart, Long> {
     Optional<Cart> findByUserAndStatus(User user, CartStatus status);
     Optional<Cart> findByUser(User user);
     void deleteByUserId(Long userId);
+
 }
